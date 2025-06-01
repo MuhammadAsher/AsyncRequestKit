976501cf2bc84fc8ad886e2ab01d0cd505b50393
Muhammad Asher Azeem <masher@ogoul.com>
2025-06-02 02:26:48 +0500

    added combine executor

--- Sources/AsyncRequestKit/Core/AsyncRequest+Execute.swift
+++ Sources/AsyncRequestKit/Core/AsyncRequest+Execute.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import Combine
 
 public extension AsyncRequest {
+    
     func execute() async throws -> Response {
         try await RequestExecutor.execute(self)
+    }
+    
+    func combineExecute() -> AnyPublisher<Response, Error> {
+        CombineRequestExecutor.execute(self)
     }
 }
