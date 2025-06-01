1612ce3a9eac99a950932e7255b81eefc54d317c
Muhammad Asher Azeem <masher@ogoul.com>
2025-06-01 21:03:29 +0500

    Added
    1- Extensions
    2- PropertyWrappers

--- Sources/AsyncRequestKit/Extensions/Encodable+ToDictionary.swift
+++ Sources/AsyncRequestKit/Extensions/Encodable+ToDictionary.swift
@@ -5,3 +5,12 @@
 //  Created by Asher Azeem on 01/06/2025.
 //
 
+import Foundation
+
+public extension Encodable {
+    func toDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        let json = try JSONSerialization.jsonObject(with: data)
+        return json as? [String: Any] ?? [:]
+    }
+}
--- Sources/AsyncRequestKit/PropertyWrapper/BodyParam.swift
+++ Sources/AsyncRequestKit/PropertyWrapper/BodyParam.swift
@@ -5,3 +5,15 @@
 //  Created by Asher Azeem on 01/06/2025.
 //
 
+import Foundation
+
+@propertyWrapper
+public struct BodyParam<T: Encodable> {
+    public let wrappedValue: T
+    public let key: String
+
+    public init(wrappedValue: T, _ key: String) {
+        self.wrappedValue = wrappedValue
+        self.key = key
+    }
+}
