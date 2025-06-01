7efabf0ed05ce7bef722fe1e0151f77306806230
Muhammad Asher Azeem <masher@ogoul.com>
2025-06-02 02:13:59 +0500

    neglet wrapped value from initial parameters

--- Sources/AsyncRequestKit/PropertyWrapper/QueryParam.swift
+++ Sources/AsyncRequestKit/PropertyWrapper/QueryParam.swift
@@ -11,14 +11,22 @@ import Foundation
 public struct QueryParam<Value: LosslessStringConvertible> {
     public var wrappedValue: Value?
     public var key: String
-    
+
+    // Explicit init for use with full syntax
     public init(wrappedValue: Value?, _ key: String) {
         self.wrappedValue = wrappedValue
         self.key = key
     }
-    
+
+    // Add this init to support simplified usage like @QueryParam("userId")
+    public init(_ key: String) {
+        self.wrappedValue = nil
+        self.key = key
+    }
+
     public var queryItem: URLQueryItem? {
         guard let value = wrappedValue else { return nil }
         return URLQueryItem(name: key, value: String(value))
     }
 }
+
